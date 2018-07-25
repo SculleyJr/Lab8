c7ccad31eec8182f63841d682d247ad8e87bf6fa
Sean Sculley <doneotreply@github.com>
2018-07-25 08:08:55 -0400

    first commit

---
+++ Lab8.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string run = "y";
+
+            while (run == "y")
+            {
+                try
+                {
+                    Console.WriteLine("Welcome to C# class! Which student would you like to learn more about? (index 1 - 10) ");
+                    var index = int.Parse(Console.ReadLine());
+                    GiveStudentInfo(index);
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Index out of range, Please try again.");
+                    continue;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a number 1 - 10 OK?");
+                    continue;
+                }
+                Console.WriteLine("Type 'Y' to learn about another student?");
+                run = Console.ReadLine();
+            }
+        }
+        private static void GiveStudentInfo(int z)
+        {
+            string[][] students = new string[3][];
+
+            students[0] = new string[11] { "does not exist", "Sean", "Sarah", "Jack", "Sam", "Bob", "Gary", "Brian", "Pam", "Frankie", "Jon" };
+            students[1] = new string[11] { "does not exist", "Miami", "Denver", "Salt Lake", "Tokyo", "Seattle", "Grand Rapids", "Grand Junction", "Chicago", "Jackson Hole", "Pyongyan" };
+            students[2] = new string[11] { "does not exist", "Apples", "Mushrooms", "Peppers", "Beans", "Prunes", "Guacamole", "Biscuits", "Tacos", "Ek Burgers", "Fried Kitty Cat" };
+
+            Console.WriteLine($"Student {z} is {students[0][z]}. What would you like to know about {students[0][z]} ? (enter 'hometown' or 'favorite food')");
+            string detail = Console.ReadLine();
+
+            while (detail.ToLower() == "hometown" || detail.ToLower() == "home town")
+            {
+                Console.WriteLine($"{students[0][z]}'s hometown is {students[1][z]}");
+                break;
+            }
+            while (detail.ToLower() == "favorite food")
+            {
+                Console.WriteLine($"{students[0][z]}'s favorite food is {students[2][z]}");
+                break;
+            }
+        }
+    }
+}
+
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Welcome to C# class! Which student would ou like to learn more about? (index 1 - 5 ");
+            int index = int.Parse(Console.ReadLine());
+
+            //if (index > 5)
+            //{
+            //    Console.WriteLine("Please try again.(enter number 1-5)");
+            //    index = int.Parse(Console.ReadLine());
+                
+            //}
+            
+           
+                string[][] students = new string[3][];
+
+                // Students 1- 5
+
+
+
+                students[0] = new string[6] { "does not exist", "sean", "sarah", "jack", "sam", "bob" };
+                students[1] = new string[6] { "does not exist", "miami", "denver", "salt lake", "tokyo", "seattle" };
+                students[2] = new string[6] { "does not exist", "apple", "mushroom", "peppers", "beans", "prunes" };
+
+            try
+            {
+
+                Console.WriteLine($"Student {index} is {students[0][index]}. What would you like to know about him? (enter 'hometown' or 'favorite food')");
+                string detail = Console.ReadLine();
+
+                while (detail.ToLower() == "hometown")
+                {
+                    Console.WriteLine($"{students[0][index]}'s hometown is {students[1][index]}");
+                    break;
+                }
+                while (detail.ToLower() == "favorite food")
+                {
+                    Console.WriteLine($"{students[0][index]}'s favorite food is {students[2][index]}");
+                    break;
+
+                }
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine("not valid input");
+                return;
+            }
+            
+
+            Console.ReadKey();
+        }
+
+    }
+}
+
+
+
+    
+               
+
